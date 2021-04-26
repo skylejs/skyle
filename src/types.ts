63437a1af30b2a19ace21c9189289a27e439dfdf
Zivsteve <zivsteve@gmail.com>
2021-04-25 20:04:23 -0400

    types(fix): box-shadow optional

--- src/types.ts
+++ src/types.ts
@@ -277,7 +277,7 @@ export type AdjustedStyles = {
   borderTopRadius?: number | string;
   borderBottomRadius?: number | string;
 
-  boxShadow: [number | string, number | string, number | string];
+  boxShadow?: [number | string, number | string, number | string];
 
   pointerEvents?: 'auto' | 'none' | 'box-only' | 'box-none';
 
