853914ca2ddfb43bf6470179c258475ee2231dd9
Zivsteve <zivsteve@gmail.com>
2021-05-02 00:51:42 -0400

    chore: css keyword mocking

---
+++ src/preprocessors/keyword.ts
@@ -0,0 +1,9 @@
+const KEYWORDS = ['inherit', 'initial', 'unset', 'none', 'auto'];
+
+export const keywordPreprocessor = (key: string, value: any) => {
+  // TODO - implement keyword functionality
+  // Right now, the keywords are ignored.
+  return {
+    [key]: KEYWORDS.includes(value.trim().toLowercase()) ? null : value,
+  };
+};
